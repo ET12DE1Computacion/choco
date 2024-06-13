6a8d4a9589e4d6113dd4cf67c16164e81aeaea0f
jvelazquez <jonathanvgms@gmail.com>
2024-06-13 11:29:31 -0300

    Fix script

--- Domains/domain.sh
+++ Domains/domain.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cd ~
+cd /root
 wget https://raw.githubusercontent.com/ET12DE1Computacion/choco/linux/Domains/hosts
-echo 'clone done!'
-echo "Franchela12" | sudo -S cp ~/hosts /etc/hosts
-echo 'copy done!'
-sudo rm ~/hosts
-echo 'remove file!'
+cp /root/hosts /etc/hosts
+rm /root/hosts
